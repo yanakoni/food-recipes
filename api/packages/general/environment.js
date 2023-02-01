83d08801ee8f5fc9b31dcc28716b9822018b55ad
Roman Palamarchuck <romanpalamarchuck@MacBook-Air-Roman.local>
2023-02-01 20:14:10 +0200

    env LOCAL_PORT

--- api/packages/general/environment.js
+++ api/packages/general/environment.js
@@ -2,7 +2,7 @@ export class Environment{
     static get config(){
         return {
             http: {
-                LOCAL_PORT: 3000
+                LOCAL_PORT: process.env.LOCAL_PORT || 3000
             }
         }
     }
