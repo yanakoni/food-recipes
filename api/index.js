484e04399052750dd0bd6489257f14a1533bd3ec
Roman <roman200220022002@gmail.com>
2023-02-04 11:08:31 +0200

    shutdown protection

--- api/index.js
+++ api/index.js
@@ -3,6 +3,10 @@ import { Environment } from "./packages/general/environment.js"
 
 const PORT = Environment.config.http.LOCAL_PORT;
 const app = express();
+app.get("/", (req, res) => {
+    res.send("<h1>it works</h1>");
+});
+
 app.listen(PORT, () => {
     console.log("Server has been started on PORT 3000...");
 });
