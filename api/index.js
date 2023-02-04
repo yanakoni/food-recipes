0e51f3fc0c64f3b9cbf31da12e29c5e74567f4ec
Roman <roman200220022002@gmail.com>
2023-02-04 12:19:51 +0200

    user integration base

--- api/index.js
+++ api/index.js
@@ -1,13 +1,15 @@
 import  express  from "express";
 import { Environment } from "./packages/general/environment.js"
+import userRouter from "./routes/userroutes"
 
 const PORT = Environment.config.http.LOCAL_PORT;
 const app = express();
 
-app.get("/", (req, res) => {
+app.use('/api/user', userRouter);
+app.get('/', (req, res) => {
     res.send("<h1>it works!!!!!!</h1>");
 });
 
 app.listen(PORT, () => {
-    console.log("Server has been started on PORT 3000...");
+    console.log(`Server has been started on ${PORT}...`);
 });
--- api/routes/userroutes.js
+++ api/routes/userroutes.js
@@ -1,7 +1,13 @@
 import { Router } from "express";
-import { UserController } from "../controllers/usercontroller.js";
+import  UserController  from "../controllers/usercontroller";
 
 const router = new Router();
 
+
+router.get('/getusers', UserController.getUsers);
+router.get('/getsingleuser/:id', UserController.getUser);
+router.post('/createuser', UserController.createUser);
+router.put('/updateuser', UserController.updateUser);
+router.delete('/deleteuser/::id', UserController.deleteUser);
 
 module.exports = router
