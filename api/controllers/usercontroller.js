cd83feafca048e455b9490e0deff34cee54816ea
Roman <roman200220022002@gmail.com>
2023-02-04 22:38:46 +0200

    jwt authorization

--- api/controllers/usercontroller.js
+++ api/controllers/usercontroller.js
@@ -1,9 +1,18 @@
 import { PrismaClient } from '@prisma/client'
 import bcrypt from "bcrypt";
+import jwt from 'jsonwebtoken';
 const prisma = new PrismaClient()
 
 export class UserController {
     async test(req, res){ 
+        const username = "roman";
+        const password = "12345";
+        const user = await prisma.users.findFirst({
+                where: {
+                    username: username
+                }
+        });
+        console.log(user)
         res.status(200).json({
             message: "111111"
         })
@@ -57,6 +66,7 @@ export class UserController {
                     username: username
                 }
             });
+            console.log(user);
             if(!user) {
                 return res.status(404).json({
                 message: "User Not found."
@@ -92,7 +102,7 @@ export class UserController {
         catch(e){
             console.warn(e);
             res.status(404).json({
-                message: "User Not found."
+                message: "User Not found catch module."
             });
         }
     }
